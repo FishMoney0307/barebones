7491fe1a4f16ff1084d00025e2cd3250e197aa93
Ethan <ethan2002@optonline.net>
2024-03-31 10:28:55 -0400

    initial project push
    i am learning so hard rn lfg

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{backgroundColor: "#404060", width: "100%"}}>
+        <h1>References</h1>
+        <a href='https://react.dev/'>React</a>
+    </div>  
+  )
+}
+
+export default Footer
