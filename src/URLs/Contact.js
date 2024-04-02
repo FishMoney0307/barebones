debc5afc2e41dca833806d2c1db11ab1118d7231
Ethan <ethan2002@optonline.net>
2024-04-02 11:42:41 -0400

    Contact Page lookin good
    now for the damn list for the database

--- src/URLs/Contact.js
+++ src/URLs/Contact.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Hero from '../Hero.js';
+import Footer from '../Footer.js';
+import './About.css';
+import './Home.css';
+import CB from './ContactBody.js';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="background">
+      <Hero />
+      <div className="Home">
+        <CB />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
---
+++ src/URLs/ContactBody.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../Body.css';
+
+const ContactBody = () => {
+  return (
+    <div className="bigContainer">
+        <div>
+            <h1>Developer</h1>
+            <p>Stuff</p>
+        </div>
+        <div>
+            <h1>Contact Information</h1>
+            <p>Email: ethan2002@optonline.net</p>
+            <p>Github: </p>
+            <p>LinkedIn: </p>
+        </div>
+    </div>
+  )
+}
+
+export default ContactBody
